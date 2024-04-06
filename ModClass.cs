d367fcf7cc9e3b92ad8f9704011463143c637f5d
Plutzz <benjiongco@gmail.com>
2024-04-06 15:25:59 -0700

    Add Logs to track false knight progress

--- ModClass.cs
+++ ModClass.cs
@@ -10,18 +10,54 @@ namespace BossSync
     public class BossSync : Mod
     {
         internal static BossSync Instance;
-        new public string GetName() => "My First Mod";
+        new public string GetName() => "BossSync";
         public override string GetVersion() => "v1";
+
         public override void Initialize()
         {
             ModHooks.HeroUpdateHook += OnHeroUpdate;
+            ModHooks.OnReceiveDeathEventHook += OnEnemyDeath;
         }
+
+        private void OnEnemyDeath(EnemyDeathEffects enemyDeathEffects, bool eventAlreadyReceived, ref float? attackDirection, ref bool resetDeathEvent, ref bool spellBurn, ref bool isWatery)
+        {
+            if (eventAlreadyReceived) return;
+
+            Log("enemyDeathEffects name: " + enemyDeathEffects.name);
+        }
+
+        private void LogAllUnlockedBossScenes()
+        {
+            Log("Boss Scenes Unlocked:");
+            foreach(var bossScene in PlayerData.instance.unlockedBossScenes)
+            {
+                Log(bossScene);
+            }
+        }
+
         public void OnHeroUpdate()
         {
             if (Input.GetKeyDown(KeyCode.O))
             {
-                Log("Key Pressed");
+                LogAllUnlockedBossScenes();
+            }
+
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                Log("falseKnightDefeated = " + PlayerData.instance.falseKnightDefeated);
+                Log("killedFalseKnight = " + PlayerData.instance.killedFalseKnight);
+                Log("killsFalseKnight = " + PlayerData.instance.killsFalseKnight);
+                Log("newDataFalseKnight = " + PlayerData.instance.newDataFalseKnight);
+            }
+
+            if(Input.GetKeyDown(KeyCode.I))
+            {
+                PlayerData.instance.SetBool("falseKnightDefeated", true);
+                PlayerData.instance.SetBool("killedFalseKnight", true);
+                PlayerData.instance.SetInt("killsFalseKnight", 0);
+                PlayerData.instance.SetBool("newDataFalseKnight", true);
             }
         }
+
     }
 }
