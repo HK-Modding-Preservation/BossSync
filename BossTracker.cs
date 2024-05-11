f6ba4deb1c4a093a6b30f19373b1ce5e2a098e4f
Plutzz <benjiongco@gmail.com>
2024-05-11 16:48:46 -0700

    Attempting Vanilla Rando Method

--- BossTracker.cs
+++ BossTracker.cs
@@ -12,6 +12,10 @@ namespace BossTracker
     public class BossTracker : Mod, IGlobalSettings<GlobalSettings>
     {
         internal static BossTracker Instance;
+        public BossTracker()
+        {
+            Instance = this;
+        }
         new public string GetName() => "BossSync";
         public override string GetVersion() => "1.0.1";
 
@@ -28,6 +32,12 @@ namespace BossTracker
             ModHooks.HeroUpdateHook += OnHeroUpdate;
             ModHooks.OnReceiveDeathEventHook += OnEnemyDeath;
             Menu.Hook();
+            Tracker.Hook();
+
+            if (ModHooks.GetMod("RandoSettingsManager") is not null)
+            {
+                RSMInterop.Hook();
+            }
         }
 
         private void OnEnemyDeath(EnemyDeathEffects enemyDeathEffects, bool eventAlreadyReceived, ref float? attackDirection, ref bool resetDeathEvent, ref bool spellBurn, ref bool isWatery)
@@ -55,19 +65,34 @@ namespace BossTracker
 
             if (Input.GetKeyDown(KeyCode.P))
             {
-                Log("falseKnightDefeated = " + PlayerData.instance.falseKnightDefeated);
-                Log("killedFalseKnight = " + PlayerData.instance.killedFalseKnight);
-                Log("killsFalseKnight = " + PlayerData.instance.killsFalseKnight);
-                Log("newDataFalseKnight = " + PlayerData.instance.newDataFalseKnight);
+                LogFalseKnight();
             }
 
             if (Input.GetKeyDown(KeyCode.I))
             {
-                PlayerData.instance.SetBool("falseKnightDefeated", true);
-                PlayerData.instance.SetBool("killedFalseKnight", true);
-                PlayerData.instance.SetInt("killsFalseKnight", 0);
-                PlayerData.instance.SetBool("newDataFalseKnight", true);
+                KillFalseKnight();
             }
+        }
+
+        internal void LogFalseKnight()
+        {
+            Log("falseKnightDefeated = " + PlayerData.instance.falseKnightDefeated);
+            Log("killedFalseKnight = " + PlayerData.instance.killedFalseKnight);
+            Log("killsFalseKnight = " + PlayerData.instance.killsFalseKnight);
+            Log("newDataFalseKnight = " + PlayerData.instance.newDataFalseKnight);
+        }
+
+        internal void KillFalseKnight()
+        {
+            PlayerData.instance.SetBool("falseKnightDefeated", true);
+            PlayerData.instance.SetBool("killedFalseKnight", true);
+            PlayerData.instance.SetInt("killsFalseKnight", 0);
+            PlayerData.instance.SetBool("newDataFalseKnight", true);
+        }
+
+        internal void CustomLog(string log)
+        {
+            Log(log);
         }
     }
 }
