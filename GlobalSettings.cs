9cb8d665a0fe345e1426d0fd3b126d2ad7de3603
Plutzz <benjiongco@gmail.com>
2024-05-11 15:47:07 -0700

    Create buttons on connections menu

---
+++ GlobalSettings.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace BossTracker
+{
+    // More or less copied from homothety's RandomizerMod PoolSettings
+    public class GlobalSettings
+    {
+        public bool Bosses;
+
+        private static readonly Dictionary<string, FieldInfo> fields = typeof(GlobalSettings)
+            .GetFields(BindingFlags.Public | BindingFlags.Instance)
+            .Where(f => f.FieldType == typeof(bool))
+            .ToDictionary(f => f.Name, f => f);
+
+        public bool GetFieldByName(string fieldName)
+        {
+            if (fields.TryGetValue(fieldName, out FieldInfo field))
+            {
+                return (bool)field.GetValue(this);
+            }
+            return false;
+        }
+
+        public Dictionary<string, bool> trackInteropPool = new();
+
+        public bool AnyEnabled()
+        {
+            return fields.Keys.Any(f => GetFieldByName(f)) || trackInteropPool.Values.Any(interop => interop);
+        }
+
+        /// <summary>
+        /// Only copies over enabled interop pools for currently registered connection mods.
+        /// </summary>
+        public static GlobalSettings MinimalClone(GlobalSettings gs)
+        {
+            GlobalSettings gsClone = (GlobalSettings)gs.MemberwiseClone();
+
+            gsClone.trackInteropPool = new();
+            foreach (KeyValuePair<string, bool> kvp in gs.trackInteropPool)
+            {
+                if (BossTracker.Instance.Interops.ContainsKey(kvp.Key) && kvp.Value)
+                {
+                    gsClone.trackInteropPool[kvp.Key] = true;
+                }
+            }
+
+            return gsClone;
+        }
+    }
+}
---
+++ RSMInterop.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+using MenuChanger.MenuElements;
+using RandoSettingsManager;
+using RandoSettingsManager.SettingsManagement;
+using RandoSettingsManager.SettingsManagement.Versioning;
+using BossTracker;
+
+
+// More or less copied from RandoVanillaTracker
+
+namespace BossTracker
+{
+    internal static class RSMInterop
+    {
+        public static void Hook()
+        {
+            RandoSettingsManagerMod.Instance.RegisterConnection(new RVTSettingsProxy());
+        }
+    }
+
+    internal class RVTSettingsProxy : RandoSettingsProxy<GlobalSettings, string>
+    {
+        public override string ModKey => BossTracker.Instance.GetName();
+
+        public override VersioningPolicy<string> VersioningPolicy { get; }
+            = new EqualityVersioningPolicy<string>(BossTracker.Instance.GetVersion());
+
+        public override void ReceiveSettings(GlobalSettings settings)
+        {
+            if (settings is not null)
+            {
+                HashSet<string> invalidPools = new();
+
+                // Validate interop settings
+                foreach (KeyValuePair<string, bool> kvp in settings.trackInteropPool)
+                {
+                    ToggleButton button = Menu.Instance.btInteropButtons.Find(button => button.Name == kvp.Key);
+                    if (button is null && kvp.Value)
+                    {
+                        invalidPools.Add(kvp.Key);
+                    }
+                }
+
+                if (invalidPools.Count > 0)
+                {
+                    throw new ValidationException($"Connection mods are missing for the following pool settings in BossTracker: {string.Join(", ", invalidPools)}");
+                }
+
+                // Set vanilla settings
+                Menu.Instance.btMEF.SetMenuValues(settings);
+
+                // Set interop settings
+                foreach (ToggleButton b in Menu.Instance.btInteropButtons)
+                {
+                    if (settings.trackInteropPool.TryGetValue(b.Name, out bool value))
+                    {
+                        // This should be true if the settings were properly provided, but just in case...
+                        b.SetValue(value);
+                    }
+                    else
+                    {
+                        b.SetValue(false);
+                    }
+                }
+            }
+            else
+            {
+                // Turn off everything except for pools from interop mods that aren't currently registered/installed
+                // (and therefore are left alone to preserve their setting)
+                foreach (IValueElement e in Menu.Instance.btMEF.Elements)
+                {
+                    e.SetValue(false);
+                }
+
+                foreach (ToggleButton b in Menu.Instance.btInteropButtons)
+                {
+                    b.SetValue(false);
+                }
+            }
+        }
+
+        public override bool TryProvideSettings(out GlobalSettings settings)
+        {
+            settings = GlobalSettings.MinimalClone(BossTracker.GS);
+            return settings.AnyEnabled();
+        }
+    }
+}
